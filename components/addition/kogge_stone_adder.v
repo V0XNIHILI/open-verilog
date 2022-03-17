f1e75716993892907563dc4974d391ae710c4b28
Douwe den Blanken <douwedb@gmail.com>
2022-03-17 11:56:48 +0100

    Add V2 of Kogge-Stone adder, unfortunately still not fully working

--- components/addition/kogge_stone_adder.v
+++ components/addition/kogge_stone_adder.v
@@ -26,19 +26,22 @@ module kogge_stone_adder
 
     parameter HEIGHT = $clog2(WIDTH);
 
-    wire intermediate_gs [HEIGHT+1:0] [WIDTH-1:0];
-    wire intermediate_ps [HEIGHT+1:0] [WIDTH-1:0];
+    wire initial_gs [WIDTH-2:0];
+    wire initial_ps [WIDTH-2:0];
+
+    wire final_gs [WIDTH-1:0];
+    wire final_ps [WIDTH-1:0];
 
     genvar i;
     generate 
-        for (i = 0; i < WIDTH; i = i + 1) 
+        for (i = 0; i < WIDTH; i = i + 1)
             begin
                 half_adder half_adder_inst
                 ( 
                     .a(a[i]),
                     .b(b[i]),
-                    .sum(intermediate_ps[0][i]),
-                    .carry_out(intermediate_ps[0][i])
+                    .sum(i == 0 ? final_ps[i] : initial_ps[i-1]),
+                    .carry_out(i == 0 ? final_gs[i] : initial_gs[i-1])
                 );
             end
     endgenerate
@@ -46,39 +49,39 @@ module kogge_stone_adder
     genvar j;
     generate 
         for (j = 0; j < HEIGHT; j = j + 1) 
-            begin
-                parameter LEVEL_WIDTH = $pow(2, j);
+            begin: stage
+                parameter LEVEL_OFFSET = $rtoi($pow(2, j));
+                parameter LEVEL_WIDTH = WIDTH - LEVEL_OFFSET;
+
+                wire intermediate_gs [LEVEL_WIDTH-1:0];
+                wire intermediate_ps [LEVEL_WIDTH-1:0];     
 
                 genvar i;
-                for (i = 0; i < WIDTH; i = i + 1) 
+                for (i = 0; i < LEVEL_WIDTH; i = i + 1) 
                 begin
-                    if (i < LEVEL_WIDTH) begin
-                        assign intermediate_ps[j+1][i] = intermediate_ps[j][i];
-                        assign intermediate_gs[j+1][i] = intermediate_gs[j][i];
-                    end else
-                        kogge_stone_cell kogge_stone_cell_inst
-                        ( 
-                            .p_i(intermediate_ps[j][i]),
-                            .g_i(intermediate_gs[j][i]),
-                            .p_i_prev(intermediate_ps[j][i-1]),
-                            .g_i_prev(intermediate_gs[j][i-1]),
-                            .p(intermediate_ps[j+1][i]),
-                            .g(intermediate_gs[j+1][i])
-                        );
+                    kogge_stone_cell kogge_stone_cell_inst
+                    ( 
+                        .p_i(j == 0 ? initial_ps[i+LEVEL_OFFSET-1] : stage[j-1].intermediate_ps[i+LEVEL_OFFSET/2]),
+                        .g_i(j == 0 ? initial_gs[i+LEVEL_OFFSET-1] : stage[j-1].intermediate_gs[i+LEVEL_OFFSET/2]),
+                        .p_i_prev(j == 0 ? (i == 0 ? final_ps[i] : initial_ps[i+LEVEL_OFFSET-2]) : (i-LEVEL_OFFSET/2 < 0 ? final_ps[i] : stage[j-1].intermediate_ps[i-LEVEL_OFFSET/2])),
+                        .g_i_prev(j == 0 ? (i == 0 ? final_gs[i] : initial_gs[i+LEVEL_OFFSET-2]) : (i-LEVEL_OFFSET/2 < 0 ? final_gs[i] : stage[j-1].intermediate_gs[i-LEVEL_OFFSET/2])),
+                        .p(j == HEIGHT - 1 ? final_ps[i+LEVEL_OFFSET] : (i - LEVEL_OFFSET < 0 ? final_ps[i+LEVEL_OFFSET] : intermediate_ps[i])),
+                        .g(j == HEIGHT - 1 ? final_gs[i+LEVEL_OFFSET] : (i - LEVEL_OFFSET < 0 ? final_gs[i+LEVEL_OFFSET]: intermediate_gs[i]))
+                    );
                 end
             end
     endgenerate
 
-    assign sum[0] = intermediate_ps[0][0] ^ 0; // No carry in is assumed
+    assign sum[0] = final_ps[0]; // Same as  initial_ps[0] ^ 0. No carry in is assumed
 
     genvar i;
     generate 
         for (i = 1; i < WIDTH; i = i + 1) 
             begin
-                assign sum[i] = intermediate_ps[HEIGHT][i] ^ intermediate_gs[0][i-1];
+                assign sum[i] = initial_ps[i-1] ^ final_gs[i-1];
             end
     endgenerate
 
-    assign carry_out = intermediate_gs[0][WIDTH-1];
+    assign carry_out = final_gs[WIDTH-1];
     
 endmodule
