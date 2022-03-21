a2b41f1da07e309cc5359b4ddced0d2d01877bd6
Douwe den Blanken <douwedb@gmail.com>
2022-03-21 21:33:24 +0100

    Create bit serial adder

---
+++ src/addition/bit_serial_adder.v
@@ -0,0 +1,34 @@
+`include "full_adder.v"
+
+module bit_serial_adder 
+    (
+        input clk,
+        input reset, 
+        input a,
+        input b, 
+        output sum
+    );
+
+    reg carry_in;
+
+    wire carry_out;
+
+    full_adder full_adder_inst
+    ( 
+        .a(a),
+        .b(b),
+        .carry_in(carry_in),
+        .sum(sum),
+        .carry_out(carry_out)
+    );
+
+    always@(posedge clk)
+    begin
+        if(reset == 1) begin //active high reset
+            carry_in = 1'b0;
+        end else begin
+            carry_in = carry_out;
+        end 
+    end
+
+endmodule
