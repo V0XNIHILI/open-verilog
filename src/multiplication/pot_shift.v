5221f04ad0c541b2c30e86779b6a501b3150cd26
Douwe den Blanken <douwedb@gmail.com>
2023-04-13 21:45:21 +0200

    Create V1 of POT shift unit

---
+++ src/multiplication/pot_shift.v
@@ -0,0 +1,22 @@
+module pot_shift
+    #(parameter WEIGHT_BIT_WIDTH = 4, parameter INPUT_BIT_WIDTH = 4)
+    (
+        input signed [INPUT_BIT_WIDTH-1:0] in,
+        input [WEIGHT_BIT_WIDTH-1:0] weight,
+        output signed [OUTPUT_BIT_WIDTH-1:0] out
+    );
+
+    // Output bit width is the sum of input bit width and maximum possible
+    // right shift, which is equal to 2^WEIGHT_BIT_WIDTH for each
+    parameter OUTPUT_BIT_WIDTH = INPUT_BIT_WIDTH+2**WEIGHT_BIT_WIDTH;
+
+    wire weight_sign = weight[WEIGHT_BIT_WIDTH-1];
+    wire [WEIGHT_BIT_WIDTH-2:0] weight_abs = weight[WEIGHT_BIT_WIDTH-2:0];
+
+    wire [OUTPUT_BIT_WIDTH-1:0] in_pushed_to_left = {in, {OUTPUT_BIT_WIDTH-INPUT_BIT_WIDTH{1'b0}}};
+
+    wire signed [OUTPUT_BIT_WIDTH-1:0] out_abs = in_pushed_to_left >>> weight_abs;
+
+    assign out = weight_sign == 1'b1 ? -out_abs : out_abs;
+
+endmodule
---
+++ src/multiplication/tb/pot_shift_tb.v
@@ -0,0 +1,53 @@
+`include "pot_shift.v"
+
+module pot_shift_tb;
+    parameter OUTPUT_BIT_WIDTH = 20;
+    parameter WEIGHT_BIT_WIDTH = 4;
+    parameter INPUT_BIT_WIDTH = 4;
+
+    integer required_shift = OUTPUT_BIT_WIDTH - WEIGHT_BIT_WIDTH;
+
+    integer value2;
+    integer value3;
+
+    reg signed [INPUT_BIT_WIDTH-1:0] in = 4'b0000;
+    reg [WEIGHT_BIT_WIDTH-1:0] weight = 4'b1000;
+    wire signed [OUTPUT_BIT_WIDTH-1:0] out;
+
+    task print_if_failed;
+        input integer expected;
+        input integer actual;
+        begin
+            if (actual !== expected) begin
+                $display("Failed. Expected: %d, actual: %d", expected, actual);
+                $stop;
+            end
+        end
+    endtask
+
+    pot_shift #(.WEIGHT_BIT_WIDTH(WEIGHT_BIT_WIDTH), .INPUT_BIT_WIDTH(INPUT_BIT_WIDTH)) pot_shift_inst
+    (
+        .in(in),
+        .weight(weight),
+        .out(out)
+    );
+
+    initial begin
+        // Check zero input handling
+        weight = 4'b0000; in = 4'b0000; # 0;
+        print_if_failed(0, out);
+
+        weight = 4'b0001; in = 4'b0000; # 0;
+        print_if_failed(0, out);
+
+        weight = 4'b1001; in = 4'b0000; # 0;
+        print_if_failed(0, out);
+        
+        // Check identity (weight is 1, so no shift)
+        weight = 4'b0000; in = 4'b0001; # 0;
+        print_if_failed(in<<required_shift, out);
+
+        weight = 4'b0000; in = 4'b1101; # 0;
+        print_if_failed(in<<required_shift, out);
+    end
+endmodule
