cadf93ffdc18a6dd0f461e187ea08a16c9cc6c26
Douwe den Blanken <douwedb@gmail.com>
2023-04-15 22:19:46 +0200

    Simplify single port SRAM tests

--- src/sram/tb/single_port_sram_tb.v
+++ src/sram/tb/single_port_sram_tb.v
@@ -1,8 +1,8 @@
 `include "single_port_sram.v"
 
 module single_port_sram_tb;
-    parameter WIDTH = 32;
-    parameter DEPTH = 16;
+    localparam WIDTH = 32;
+    localparam DEPTH = 16;
     localparam ADDR_WIDTH = $clog2(DEPTH);
 
     /* Make a regular pulsing clock. */
@@ -35,6 +35,42 @@ module single_port_sram_tb;
             end
         end
     endtask
+
+    task fill_sram;
+        input local_we;
+        input local_cs;
+        input local_oe;
+        input write_zeros;
+        begin
+            for (integer i = 0; i < DEPTH; i = i + 1) begin
+                repeat (1) @ (posedge clk) address <= i; we <= local_we; cs <= local_cs; oe <= local_oe; data_reg <= write_zeros ? 0 : random_numbers[i];
+            end
+        end
+    endtask
+
+    task check_data_present;
+        input local_we;
+        input local_cs;
+        input local_oe;
+        begin
+            // Check if the original random numbers are still there.
+            for (integer i = 0; i < DEPTH + 2; i = i + 1) begin
+                if (i < DEPTH) begin
+                    repeat (1) @ (posedge clk) address <= i;  we <= local_we; cs <= local_cs; oe <= local_oe;
+                end else begin
+                    repeat (1) @ (posedge clk);
+                end
+
+                if (i >= 2)
+                    print_if_failed(local_oe ? random_numbers[i-2] : data_reg, data);
+            end
+        end
+    endtask
+
+    initial begin
+        $dumpfile("single_port_sram_tb.vcd");
+        $dumpvars(0,clk,data,address,cs,we,oe);
+    end
     
     single_port_sram #(.WIDTH(WIDTH), .DEPTH(DEPTH)) single_port_sram_inst
     (
@@ -46,66 +82,28 @@ module single_port_sram_tb;
         .output_enable(oe)
     );
 
-    initial begin
-        $dumpfile("single_port_sram_tb.vcd");
-        $dumpvars(0,clk,data,address,cs,we,oe);
-    end
-
     assign data = !oe ? data_reg : 'hz;
 
     initial begin
         repeat (1) @ (posedge clk);
 
         // Write random numbers to the SRAM
-        for (integer i = 0; i < DEPTH; i = i + 1) begin
-            repeat (1) @ (posedge clk) address <= i; we <= 1; cs <= 1; oe <= 0; data_reg <= random_numbers[i];
-        end
+        fill_sram(1, 1, 0, 0);
 
         // Try to write zeros to the SRAM while chip select is low.
-        for (integer i = 0; i < DEPTH; i = i + 1) begin
-            repeat (1) @ (posedge clk) address <= i; we <= 1; cs <= 0; oe <= 0; data_reg <= 0;
-        end
+        fill_sram(1, 0, 0, 1);
 
         // Check that the SRAM still contains the random numbers.
-        for (integer i = 0; i < DEPTH + 2; i = i + 1) begin
-            if (i < DEPTH) begin
-                repeat (1) @ (posedge clk) address <= i; we <= 0; cs <= 1; oe <= 1;
-            end else begin
-                repeat (1) @ (posedge clk);
-            end
-
-            if (i >= 2)
-                print_if_failed(random_numbers[i-2], data);
-        end
+        check_data_present(0, 1, 1);
 
         // Try to write zeros to the SRAM while write enable is low
-        for (integer i = 0; i < DEPTH; i = i + 1) begin
-            repeat (1) @ (posedge clk) address <= i; we <= 0; cs <= 1; oe <= 0; data_reg <= 0;
-        end
+        fill_sram(0, 1, 0, 1);
 
         // Check if the original random numbers are still there.
-        for (integer i = 0; i < DEPTH + 2; i = i + 1) begin
-            if (i < DEPTH) begin
-                repeat (1) @ (posedge clk) address <= i; we <= 0; cs <= 1; oe <= 1;
-            end else begin
-                repeat (1) @ (posedge clk);
-            end
-
-            if (i >= 2)
-                print_if_failed(random_numbers[i-2], data);
-        end
+        check_data_present(0, 1, 1);
 
         // Check that read out values are 0 when output enable is low (due to assign data statement in this file)
-        for (integer i = 0; i < DEPTH + 2; i = i + 1) begin
-            if (i < DEPTH) begin
-                repeat (1) @ (posedge clk) address <= i; we <= 0; cs <= 1; oe <= 0;
-            end else begin
-                repeat (1) @ (posedge clk);
-            end
-
-            if (i >= 2)
-                print_if_failed(0, data);
-        end
+        check_data_present(0, 1, 0);
 
         $finish;
     end
