62c5a2103b52b4bff7f195dc61848eb218e80b9c
Douwe den Blanken <douwedb@gmail.com>
2023-04-15 22:54:30 +0200

    Simplify ReLU

--- src/machine_learning/relu.v
+++ src/machine_learning/relu.v
@@ -1,11 +1,11 @@
 module relu
-    #(parameter WIDTH = 8, parameter ZERO = 0)
+    #(parameter WIDTH = 8)
 	(
 		input sign,
 		input [WIDTH-1:0] a,
 		output [WIDTH-1:0] z,
 	);
 
-	assign z = (sign == 1'b1) ? ZERO : sum;
+	assign z = (sign == 1'b1) ? 0 : a;
 	
 endmodule
