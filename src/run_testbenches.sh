52edb2b1c89bf7bdf4ca5a53c6653ec09435fa0c
Douwe den Blanken <douwedb@gmail.com>
2023-04-15 22:53:35 +0200

    Create initial version of GH actions runner for tests

---
+++ src/run_testbenches.sh
@@ -0,0 +1,24 @@
+# Make a list of all (nested) files that end in _tb.v
+# and run them through the testbench generator
+
+TB_LIST=`find . -name "*_tb.v" | sort`
+
+for tb in $TB_LIST
+do
+    FILE_NAME=`basename $tb`
+    echo "Running testbench for $FILE_NAME"
+    
+    # cd into the directory of the testbench
+    cd `dirname $tb`/../
+
+    iverilog -o "$FILE_NAME.iv" ./tb/$FILE_NAME && vvp -N "$FILE_NAME.iv"
+    
+    EXIT_CODE=$?
+
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "Testbench failed with exit code $EXIT_CODE"
+        exit $EXIT_CODE
+    fi
+    
+    cd ../
+done
