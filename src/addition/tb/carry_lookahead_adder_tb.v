8a38b22c500549b18dabd117c70ce9b5b888e72e
Douwe den Blanken <douwedb@gmail.com>
2023-04-17 18:23:12 +0200

    Also check carry_in on carry lookahead adder

--- src/addition/tb/carry_lookahead_adder_tb.v
+++ src/addition/tb/carry_lookahead_adder_tb.v
@@ -3,6 +3,7 @@
 module carry_lookahead_adder_tb;
     localparam WIDTH = 8;
 
+    reg carry_in;
     reg [WIDTH-1:0] a;
     reg [WIDTH-1:0] b;
     wire carry_out;
@@ -11,7 +12,7 @@ module carry_lookahead_adder_tb;
 
     carry_lookahead_adder #(.WIDTH(WIDTH)) kogge_stone_adder_inst
     (
-        .carry_in(1'b0),
+        .carry_in(carry_in),
         .a(a),
         .b(b),
         .sum(sum),
@@ -38,14 +39,21 @@ module carry_lookahead_adder_tb;
     initial begin
         for (integer i = 0; i < 2 ** WIDTH; i = i + 1) begin
             a = i;
+
             for (integer j = 0; j < 2 ** WIDTH; j = j + 1) begin
                 b = j;
-                # 1;
-                if (i + j >= 2 ** WIDTH) begin
-                    print_if_failed(i + j - 2 ** WIDTH, sum);
-                    print_if_failed(1, carry_out);
-                end else
-                    print_if_failed(i + j, sum);
+                
+                for (integer k = 0; k < 1; k = k + 1) begin
+                    carry_in = k;
+
+                    # 1;
+
+                    if (i + j + k >= 2 ** WIDTH) begin
+                        print_if_failed(i + j  + k - 2 ** WIDTH, sum);
+                        print_if_failed(1, carry_out);
+                    end else
+                        print_if_failed(i + j + k, sum);
+                end
             end
         end
 
